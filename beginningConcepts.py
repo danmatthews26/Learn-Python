054156f600ab5913f5f55046cc2349128dd85b20
Danny Matthews <matthewsdan629@gmail.com>
2017-06-09 16:17:46 -0400

    This file contains basic arithmatic and the print function

---
+++ beginningConcepts.py
@@ -0,0 +1,49 @@
+# NOTE: It is best to play around with the different things in this file
+# by using a seperate file.
+# NOTE: There are no semi-colons, or brackets in Python.
+
+'''
+This is a multi line comment, nothing in here will be read by the compiler
+print("hey")
+See, this did not print when the file was compiled and ran
+'''
+# This is a single line comment. This will not be read by the compiler
+
+# PRINT FUNCTION
+# This is the print function.
+# leaving the print function empty will creat a line break
+print()
+
+# To put a String into the print function,
+# you simply use single or double quote marks, and put the words inside.
+# NOTE: If you start with double quotes, you must end with a double quote,
+# same rule with single quotes
+# print("hey") - valid
+# print('hey') - valid
+# print("hey') - not valid
+# print('hey") - not valid
+
+print('hello with single quotation mark')
+print("hello with double quotation marks")
+print()
+
+# Basic python arithmatic
+# Addition- +
+# Subtraction- -
+# Multiplication- *
+# Division- /
+# Get the remainder of a division problem- %
+# Exponets - **
+# Floor Division- // (performs a division problem and discards the remainder)
+
+print("13 + 4 =", 13+4)
+print("11 - 2 =", 11-2)
+print("10 x 10 =", 10*10)
+print("100 % 10 =", 100/10)
+print("Remainder of 11 / 5 =", 11%5)
+print("5 to the power of 3 =", 5**3)
+print("11 / 3 without the remainder =", 11//3)
+print()
+
+
+
